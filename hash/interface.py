d7e03b6cffa2d834c7b8133e307da9a132fef18a
Amanda Gois <amandabalcacar@gmail.com>
2024-09-02 16:19:08 -0400

    centralizado e funcionando

--- hash/interface.py
+++ hash/interface.py
@@ -1,27 +1,65 @@
 import tkinter as tk
 from tkinter import filedialog
+import hashlib
+
+def gerar_hash(conteudo, algoritmo):
+    hash_obj = hashlib.new(algoritmo)
+    hash_obj.update(conteudo)
+    return hash_obj.hexdigest()
 
 def abrir_arquivo():
-    # Abre o diálogo para seleção de arquivo
     caminho_arquivo = filedialog.askopenfilename(
         title="Selecione o arquivo", 
-        filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
+        filetypes=[("All files", "*.*")]
     )
     
-    # Verifica se um arquivo foi selecionado
     if caminho_arquivo:
-        with open(caminho_arquivo, 'r') as arquivo:
+        with open(caminho_arquivo, 'rb') as arquivo: 
             conteudo = arquivo.read()
-            print(conteudo)  # Aqui você pode manipular o conteúdo do arquivo como preferir
+            algoritmo_selecionado = var_algoritmo.get()
+            hash_resultado = gerar_hash(conteudo, algoritmo_selecionado)
+            print(f"{algoritmo_selecionado.upper()}: {hash_resultado}")
+            entry_hash.delete(0, tk.END)  
+            entry_hash.insert(0, hash_resultado)  
 
-# Configura a janela principal do Tkinter
+def copiar_hash():
+    janela.clipboard_clear()
+    janela.clipboard_append(entry_hash.get())
+    janela.update() 
+    print("Hash copiada para a área de transferência!")
+
+#Organização da Tela
 janela = tk.Tk()
-janela.title("Leitor de Arquivo")
-janela.geometry("400x300")
+janela.title("Leitor de Arquivo e Gerador de Hash")
+janela.geometry("450x230")
+frame_principal = tk.Frame(janela)
+frame_principal.pack(expand=True)
+frame_opcao = tk.Frame(frame_principal)
+frame_opcao.pack(pady=10)
 
-# Adiciona um botão para abrir o arquivo
-botao_abrir = tk.Button(janela, text="Abrir Arquivo", command=abrir_arquivo)
-botao_abrir.pack(pady=20)
 
-# Inicia o loop principal do Tkinter
+#Menu de Algoritmos Hash
+var_algoritmo = tk.StringVar(value="sha256")
+algoritmos = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512"]
+label_opcao = tk.Label(frame_opcao, text="Escolha o algoritmo de hash:")
+label_opcao.pack(side=tk.LEFT, padx=5)
+menu_algoritmo = tk.OptionMenu(frame_opcao, var_algoritmo, *algoritmos)
+menu_algoritmo.pack(side=tk.LEFT, padx=5)
+
+#Frame para organizar a barra de texto e o botão de copiar lado a lado
+frame_hash = tk.Frame(frame_principal)
+frame_hash.pack(pady=20)
+
+#Campo de texto
+entry_hash = tk.Entry(frame_hash, width=60) 
+entry_hash.pack(side=tk.LEFT, padx=5)
+
+#Botão
+botao_copiar = tk.Button(frame_hash, text="Copiar", command=copiar_hash)
+botao_copiar.pack(side=tk.LEFT, padx=5)
+
+botao_abrir = tk.Button(frame_principal, text="Abrir Arquivo", command=abrir_arquivo)
+botao_abrir.pack(pady=10)
+
+
 janela.mainloop()
